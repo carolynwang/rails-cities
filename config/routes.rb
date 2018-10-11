babfa7eed90b054863dfc907364c7aa988aa70b3
carolynwang <carolyn.wang@berkeley.edu>
2018-10-10 20:36:46 -0700

    finish hw2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   root "main#index"
+
+  get 'cities/view', to: 'cities#view'
+  get 'cities/new', to: 'cities#new'
+  get 'cities/update', to: 'cities#update'
+  get 'cities/delete', to: 'cities#delete'
+  post 'cities/new', to: 'cities#create'
+  post 'cities/create', to: 'cities#create'
+  post 'cities/postUpdate', to: 'cities#postUpdate'
+  post 'cities/postDelete', to: 'cities#postDelete'
+  post 'cities/main', to: 'cities#returnToHome'
 end
